03907cd8b2aa363ca8a22f8505811c0d30fc97c3
zhangyoulun <zhangyoulun@btime.com>
2017-10-16 19:36:51 +0800

    add comment

--- zookeeper-api.php
+++ zookeeper-api.php
@@ -3,109 +3,218 @@
 /**
  * Zookeeper class.
  */
+class Zookeeper
+{
 
-class Zookeeper {
+    /* class constants */
+    const PERM_READ = 1;
+    const PERM_WRITE = 2;
+    const PERM_CREATE = 4;
+    const PERM_DELETE = 8;
+    const PERM_ADMIN = 16;
+    const PERM_ALL = 31;
 
-	/* class constants */
-	const PERM_READ   = 1;
-	const PERM_WRITE  = 2;
-	const PERM_CREATE = 4;
-	const PERM_DELETE = 8;
-	const PERM_ADMIN  = 16;
-	const PERM_ALL    = 31;
+    const EPHEMERAL = 1;
+    const SEQUENCE = 2;
 
-	const EPHEMERAL = 1;
-	const SEQUENCE  = 2;
+    const EXPIRED_SESSION_STATE = -112;
+    const AUTH_FAILED_STATE = -113;
+    const CONNECTING_STATE = 1;
+    const ASSOCIATING_STATE = 2;
+    const CONNECTED_STATE = 3;
+    const NOTCONNECTED_STATE = 999;
 
-	const EXPIRED_SESSION_STATE = -112;
-	const AUTH_FAILED_STATE     = -113;
-	const CONNECTING_STATE      = 1;
-	const ASSOCIATING_STATE     = 2;
-	const CONNECTED_STATE       = 3;
-	const NOTCONNECTED_STATE    = 999;
+    const CREATED_EVENT = 1;
+    const DELETED_EVENT = 2;
+    const CHANGED_EVENT = 3;
+    const CHILD_EVENT = 4;
+    const SESSION_EVENT = -1;
+    const NOTWATCHING_EVENT = -2;
 
-	const CREATED_EVENT         = 1;
-	const DELETED_EVENT         = 2;
-	const CHANGED_EVENT         = 3;
-	const CHILD_EVENT           = 4;
-	const SESSION_EVENT         = -1;
-	const NOTWATCHING_EVENT     = -2;
+    const LOG_LEVEL_ERROR = 1;
+    const LOG_LEVEL_WARN = 2;
+    const LOG_LEVEL_INFO = 3;
+    const LOG_LEVEL_DEBUG = 4;
 
-	const LOG_LEVEL_ERROR = 1;
-	const LOG_LEVEL_WARN  = 2;
-	const LOG_LEVEL_INFO  = 3;
-	const LOG_LEVEL_DEBUG = 4;
+    const SYSTEMERROR = -1;
+    const RUNTIMEINCONSISTENCY = -2;
+    const DATAINCONSISTENCY = -3;
+    const CONNECTIONLOSS = -4;
+    const MARSHALLINGERROR = -5;
+    const UNIMPLEMENTED = -6;
+    const OPERATIONTIMEOUT = -7;
+    const BADARGUMENTS = -8;
+    const INVALIDSTATE = -9;
 
-	const SYSTEMERROR          = -1;
-	const RUNTIMEINCONSISTENCY = -2;
-	const DATAINCONSISTENCY    = -3;
-	const CONNECTIONLOSS       = -4;
-	const MARSHALLINGERROR     = -5;
-	const UNIMPLEMENTED        = -6;
-	const OPERATIONTIMEOUT     = -7;
-	const BADARGUMENTS         = -8;
-	const INVALIDSTATE         = -9;
+    const OK = 0;
+    const APIERROR = -100;
+    const NONODE = -101;
+    const NOAUTH = -102;
+    const BADVERSION = -103;
+    const NOCHILDRENFOREPHEMERALS = -108;
+    const NODEEXISTS = -110;
+    const NOTEMPTY = -111;
+    const SESSIONEXPIRED = -112;
+    const INVALIDCALLBACK = -113;
+    const INVALIDACL = -114;
+    const AUTHFAILED = -115;
+    const CLOSING = -116;
+    const NOTHING = -117;
+    const SESSIONMOVED = -118;
 
-	const OK         = 0;
-	const APIERROR   = -100;
-	const NONODE     = -101;
-	const NOAUTH     = -102;
-	const BADVERSION = -103;
-	const NOCHILDRENFOREPHEMERALS = -108;
-	const NODEEXISTS = -110;
-	const NOTEMPTY   = -111;
-	const SESSIONEXPIRED  = -112;
-	const INVALIDCALLBACK = -113;
-	const INVALIDACL      = -114;
-	const AUTHFAILED      = -115;
-	const CLOSING         = -116;
-	const NOTHING         = -117;
-	const SESSIONMOVED    = -118;
+    /**
+     * 构造函数
+     * @param string $host
+     * @param callable $watcher_cb
+     * @param int $recv_timeout
+     */
+    public function __construct($host = '', $watcher_cb = null, $recv_timeout = 10000)
+    {
+    }
 
+    /**
+     * @param $host
+     * @param $watcher_cb
+     * @param int $recv_timeout
+     */
+    public function connect($host, $watcher_cb = null, $recv_timeout = 10000)
+    {
+    }
 
-	/* if the host is provided, attempt to connect. */
-	public function __construct( $host = '', $watcher_cb = null, $recv_timeout = 10000) {}
+    /**
+     * @param $path
+     * @param $value
+     * @param $acl
+     * @param $flags
+     */
+    public function create($path, $value, $acl, $flags = null)
+    {
+    }
 
-	public function connect( $host, $watcher_cb = null, $recv_timeout = 10000) {}
+    /**
+     * @param $path
+     * @param int $version
+     */
+    public function delete($path, $version = -1)
+    {
+    }
 
-	public function create( $path, $value, $acl, $flags = null ) {}
+    /**
+     * @param $path
+     * @param $data
+     * @param int $version
+     * @param $stat
+     */
+    public function set($path, $data, $version = -1, &$stat = null)
+    {
+    }
 
-	public function delete( $path, $version = -1 ) {}
+    /**
+     * @param $path
+     * @param $watcher_cb
+     * @param $stat
+     * @param int $max_size
+     */
+    public function get($path, $watcher_cb = null, &$stat = null, $max_size = 0)
+    {
+    }
 
-	public function set( $path, $data, $version = -1, &$stat = null ) {}
+    /**
+     * @param $path
+     * @param $watcher_cb
+     */
+    public function getChildren($path, $watcher_cb = null)
+    {
+    }
 
-	public function get( $path, $watcher_cb = null, &$stat = null, $max_size = 0) {}
+    /**
+     * @param $path
+     * @param $watcher_cb
+     */
+    public function exists($path, $watcher_cb = null)
+    {
+    }
 
-	public function getChildren( $path, $watcher_cb = null ) {}
+    /**
+     * @param $path
+     */
+    public function getAcl($path)
+    {
+    }
 
-	public function exists( $path, $watcher_cb = null ) {}
+    /**
+     * @param $path
+     * @param $version
+     * @param $acls
+     */
+    public function setAcl($path, $version, $acls)
+    {
+    }
 
-	public function getAcl( $path ) {}
+    /**
+     *
+     */
+    public function getClientId()
+    {
+    }
 
-	public function setAcl( $path, $version, $acls ) {}
+    /**
+     * @param $watcher_cb
+     */
+    public function setWatcher($watcher_cb)
+    {
+    }
 
-	public function getClientId( ) {}
+    /**
+     *
+     */
+    public function getState()
+    {
+    }
 
-	public function setWatcher( $watcher_cb ) {}
+    /**
+     *
+     */
+    public function getRecvTimeout()
+    {
+    }
 
-	public function getState( ) {}
+    /**
+     * @param $scheme
+     * @param $cert
+     * @param $completion_cb
+     */
+    public function addAuth($scheme, $cert, $completion_cb = null)
+    {
+    }
 
-	public function getRecvTimeout( ) {}
+    /**
+     *
+     */
+    public function isRecoverable()
+    {
+    }
 
-	public function addAuth( $scheme, $cert, $completion_cb = null ) {}
+    /**
+     * @param $file
+     */
+    public function setLogStream($file)
+    {
+    }
 
-	public function isRecoverable( ) {}
+    /**
+     * @param $level
+     */
+    static public function setDebugLevel($level)
+    {
+    }
 
-	public function setLogStream( $file ) {} // TODO: might be able to set a stream like php://stderr or something
-
-	public function getResultMessage( ) {}
-
-
-	// static methods
-
-	static public function setDebugLevel( $level ) {}
-
-	static public function setDeterministicConnOrder( $trueOrFalse ) {}
+    /**
+     * @param $trueOrFalse
+     */
+    static public function setDeterministicConnOrder($trueOrFalse)
+    {
+    }
 
 }
 
